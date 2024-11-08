a288eadb4b9cee63823ead57e050dbdf6742dc3d
nuriamoreng <nuriamoreno01@gmail.com>
2024-11-08 13:19:31 +0100

    +

--- js/script.js
+++ js/script.js
@@ -51,6 +51,56 @@ floatingImgs.forEach((img) => {
     });
 });
 
+  // Abre el modal automáticamente después de 2 segundos
+  $(document).ready(function(){
+    setTimeout(function(){
+        $('#newsletterModal').modal('show');
+    }, 2000); // Puedes ajustar el tiempo de apertura aquí (en milisegundos)
+});
+
+// Simulación de suscripción
+$('#subscribeForm').on('submit', function(event) {
+    event.preventDefault();
+    const email = $('#emailInput').val();
+    if (email) {
+        alert(`Gracias por suscribirte con el correo: ${email}`);
+        $('#newsletterModal').modal('hide');
+    } else {
+        alert("Por favor, ingresa un correo electrónico válido.");
+    }
+});
+
+// SLIDESHOW
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 // texto que se redacta 
 var typed = new Typed('#element-1', {
     strings: ['AMAIA'],
